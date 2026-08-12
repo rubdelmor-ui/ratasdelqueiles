7dfeeeef2a3290b70dd58500083370481996a17f
ElPetiKlk <ddelmoralhernandez@gmail.com>
2026-08-12 15:49:58 +0200

    Actualización completa con Cloudinary

--- conexion.php
+++ conexion.php
@@ -1,12 +1,4 @@
 <?php
-// --- VARIABLES DE CLOUDINARY ---
-// Si no están configuradas en el servidor (ej. en local), las creamos al vuelo.
-// ¡Recuerda poner tus datos reales entre las comillas!
-if (!getenv('CLOUDINARY_CLOUD_NAME')) {
-    putenv('CLOUDINARY_CLOUD_NAME=jdy9l398');
-    putenv('CLOUDINARY_API_KEY=351577216495629');
-    putenv('CLOUDINARY_API_SECRET=Nk-iAVeRVnDJZ9dE_HS4AC84UBY');
-}
 
 // Usamos las variables de entorno de Render si existen (getenv),
 // si no existen, usa tus datos de Railway por defecto.
